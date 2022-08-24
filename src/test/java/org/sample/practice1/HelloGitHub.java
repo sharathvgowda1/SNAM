2be3086b02903632e9cb94949d2625baff046156
Nikhita Biradar <Nikhita Biradar@Nikki>
2022-08-24 16:45:30 +0530

    12323

---
+++ src/test/java/org/sample/practice1/HelloGitHub.java
@@ -0,0 +1,12 @@
+package org.sample.practice1;
+
+public class HelloGitHub {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hai this is for practice");
+		System.out.println("****++++----");
+		
+	}
+
+}
