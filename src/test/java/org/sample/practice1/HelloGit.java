0617ffdb4bf33bedf384f908d468f4490e1a1b04
HP <HP@DESKTOP-3O1JLKM>
2022-08-24 12:36:27 +0530

    Script1 and Script2

---
+++ src/test/java/org/sample/practice1/HelloGit.java
@@ -0,0 +1,10 @@
+package org.sample.practice1;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello this is for git practice");
+	}
+
+}
---
+++ src/test/java/org/sample/practice2/HelloGitHub.java
@@ -0,0 +1,11 @@
+package org.sample.practice2;
+
+public class HelloGitHub {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hai this is for practice");
+		
+	}
+
+}
